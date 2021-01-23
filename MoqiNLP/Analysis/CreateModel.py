0738e1b98385c8dc7583b10be3e158dc25e8e56d
moqi <1402290478@qq.com>
2021-01-23 21:08:05 +0800

    修改了数据编码，改为utf-8的格式

--- MoqiNLP/Analysis/CreateModel.py
+++
@@ -1,9 +0,0 @@
-from keras.layers.core import Activation, Dense
-from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM
-from keras.models import Sequential
-from keras.preprocessing import sequence
-from sklearn.model_selection import train_test_split
-import collections  #用来统计词频
-import numpy as np
-import jieba        #用来分词
