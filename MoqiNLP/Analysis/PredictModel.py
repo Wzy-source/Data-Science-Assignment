cf6234cc0fc9534264f8c745706b93d5bbe9b7b5
moqi <1402290478@qq.com>
2021-01-24 19:16:30 +0800

    修改了数据的小bug

--- MoqiNLP/Analysis/PredictModel.py
+++ MoqiNLP/Analysis/PredictModel.py
@@ -34,11 +34,11 @@ neg_review = dealData('..\WordsRepos\\EmotionWords\\negative.xls')+dealData('..\
 pos_review=pos_review[0:40000]
 neg_review=neg_review[0:32000]
 
-def jieba_feature(number):
+def build_features():
     posWords = []
     negWords = []
 
-    for items in pos_review:  # 把集合的集合变成集合
+    for items in pos_review:
         for item in items:
             posWords.append(item)
 
@@ -46,9 +46,9 @@ def jieba_feature(number):
         for item in items:
             negWords.append(item)
 
-    word_fd = nltk.FreqDist()  # 可统计所有词的词频
+    word_fd = nltk.FreqDist()
 
-    cond_word_fd = nltk.ConditionalFreqDist()  # 可统计积极文本中的词频和消极文本中的词频
+    cond_word_fd = nltk.ConditionalFreqDist()
 
     for word in posWords:
         word_fd[word] += 1
@@ -60,33 +60,30 @@ def jieba_feature(number):
 
         cond_word_fd['neg'][word] += 1
 
-    pos_word_count = cond_word_fd['pos'].N()  # 积极词的数量
+    pos_word_count = cond_word_fd['pos'].N()
 
-    neg_word_count = cond_word_fd['neg'].N()  # 消极词的数量
+    neg_word_count = cond_word_fd['neg'].N()
 
     total_word_count = pos_word_count + neg_word_count
 
-    word_scores = {}  # 包括了每个词和这个词的信息量
+    word_scores = {}
 
     for word, freq in word_fd.items():
         pos_score = nltk.BigramAssocMeasures.chi_sq(cond_word_fd['pos'][word], (freq, pos_word_count),
-                                                    total_word_count)  # 计算积极词的卡方统计量，这里也可以计算互信息等其它统计量
+                                                    total_word_count)
 
         neg_score = nltk.BigramAssocMeasures.chi_sq(cond_word_fd['neg'][word], (freq, neg_word_count),
-                                                    total_word_count)  # 同理
+                                                    total_word_count)
 
-        word_scores[word] = pos_score + neg_score  # 一个词的信息量等于积极卡方统计量加上消极卡方统计量
+        word_scores[word] = pos_score + neg_score
 
     best_vals = sorted(word_scores.items(), key=lambda item: item[1], reverse=True)[
-                :number]  # 把词按信息量倒序排序。number是特征的维度，是可以不断调整直至最优的
+                :2000]
 
     best_words = set([w for w, s in best_vals])
 
-    return dict([(word, True) for word in best_words])
+    feature = dict([(word, True) for word in best_words])
 
-
-def build_features():
-    feature = jieba_feature(2000)  # 结巴分词
     posFeatures = []
 
     for items in pos_review:
@@ -98,7 +95,7 @@ def build_features():
             if item in feature.keys():
                 a[item] = 'True'
 
-        posWords = [a, 'pos']  # 为积极文本赋予"pos"
+        posWords = [a, 'pos']
 
         posFeatures.append(posWords)
 
@@ -113,65 +110,42 @@ def build_features():
             if item in feature.keys():
                 a[item] = 'True'
 
-        negWords = [a, 'neg']  # 为消极文本赋予"neg"
+        negWords = [a, 'neg']
 
         negFeatures.append(negWords)
 
     return posFeatures, negFeatures
 
 
-posFeatures, negFeatures = build_features()  # 获得训练数据
+posFeatures, negFeatures = build_features()
 
-shuffle(posFeatures)  # 把文本的排列随机化
+shuffle(posFeatures)
 
-shuffle(negFeatures)  # 把文本的排列随机化
+shuffle(negFeatures)
 
-train = posFeatures[1000:] + negFeatures[1000:]  # 训练集(80%)
+train = posFeatures[1000:] + negFeatures[1000:]
 
-test = posFeatures[:1000] + negFeatures[:1000]  # 预测集(验证集)(20%)
+test = posFeatures[:1000] + negFeatures[:1000]
 
-data, tag = zip(*test)  # 分离测试集合的数据和标签，便于验证和测试
+data, tag = zip(*test)
 
 
 def score(classifier):
-    classifier = nltk.SklearnClassifier(classifier)  # 在nltk中使用scikit-learn的接口
-
-    classifier.train(train)  # 训练分类器
+    classifier = nltk.SklearnClassifier(classifier)
+    classifier.train(train)
     joblib.dump(classifier,'LogisticRegression.model')
-    pred = classifier.classify_many(data)  # 对测试集的数据进行分类，给出预测的标签
-
+    pred = classifier.classify_many(data)
     n = 0
-
     s = len(pred)
-
     for i in range(0, s):
-
         if pred[i] == tag[i]:
             n = n + 1
-
-    return n / s  # 对比分类预测结果和人工标注的正确结果，给出分类器准确度
-
-
-import sklearn
-
-from nltk.classify.scikitlearn import SklearnClassifier
-
-from sklearn.svm import SVC, LinearSVC, NuSVC
+    return n / s
 
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 
 from sklearn.linear_model import LogisticRegression
 
-from sklearn.metrics import accuracy_score
-
 #print('BernoulliNB`s accuracy is %f' % score(BernoulliNB()))
 
-#print('MultinomiaNB`s accuracy is %f' % score(MultinomialNB()))
-
 print('LogisticRegression`s accuracy is  %f' % score(LogisticRegression()))
-
-#print('SVC`s accuracy is %f' % score(SVC()))
-
-#print('LinearSVC`s accuracy is %f' % score(LinearSVC()))
-
-#print('NuSVC`s accuracy is %f' % score(NuSVC()))
